0ef9256195a26aac8fed5dbbb69046f33f20e707
trhuyyy13 <huytran.soict.hust@gmail.com>
2024-11-06 12:43:24 +0700

    Create admin/courses/ITxxxx, link db

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -8,15 +8,21 @@ router.get('/', adminController.getHomePage);
 
 
 router.get('/courses', adminController.getCouresPage);
-router.get('/courses/:course_id', adminController.getCourseById);
+router.get('/courses/:course_id', adminController.getClassesPage);
 
 
 // Route để xử lý đăng xuất
 router.get('/logout', adminController.logout);
 
 router.post('/search-courses', adminController.searchCourses);
+// Route để tạo khóa học
+router.post('/courses/create', adminController.createCourse);
 
+// Route để xóa khóa học
+router.post('/courses/delete', adminController.deleteCourse);
 
+// Route để cập nhật khóa học
+router.post('/courses/update', adminController.updateCourse);
 
 
 module.exports = router;
