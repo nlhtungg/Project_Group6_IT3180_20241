63efbb59907f3ebc937478b7f5d7875e0b0d26f5
Hung Manh Ngo <89698425+hungnm04@users.noreply.github.com>
2024-11-08 00:45:32 +0700

    fix adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,40 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-const isAdmin = require('./protectedRoutes');  
+const isAdmin = require('./protectedRoutes');
 
 // Protect the admin home route
 router.get('/', isAdmin, adminController.getHomePage);
 
-// Admin login routes 
+// Admin login routes
 router.get('/login', adminController.getLoginPage);
 router.post('/login', adminController.login);
 
 // Admin logout route (protected)
 router.post('/logout', isAdmin, adminController.logout);
 
-module.exports = router;
-
-// Route for home page
-router.get('/', adminController.getHomePage);
-
-
+// Route for courses
 router.get('/courses', adminController.getCouresPage);
 router.get('/courses/:course_id', adminController.getClassesPage);
 
-
-// Route để xử lý đăng xuất
-router.get('/logout', adminController.logout);
-
+// Route to search courses
 router.post('/search-courses', adminController.searchCourses);
-// Route để tạo khóa học
+
+// Route to create a course
 router.post('/courses/create', adminController.createCourse);
 
-// Route để xóa khóa học
+// Route to delete a course
 router.post('/courses/delete', adminController.deleteCourse);
 
-// Route để cập nhật khóa học
+// Route to update a course
 router.post('/courses/update', adminController.updateCourse);
-
 
 module.exports = router;
